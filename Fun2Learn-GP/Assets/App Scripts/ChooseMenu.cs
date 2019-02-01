f72d9b02fb385f169f68fce8f5913c8059b58e41
Ahmed Teleb <ahmedasd37@gmail.com>
2019-02-01 20:10:55 +0200

    Fun2Learn-AR_app
    it is app for kids teaching. It is built with Unity 3D as a game engine and targeting Android platform. Using the technology of Augmented reality. It makes learning much easier and funnier. This is the first phase which is concerned with alphabets and numbers in both languages Arabic and English.

---
+++ Fun2Learn-GP/Assets/App Scripts/ChooseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void LoadArabicLetter()
+	{
+		Application.LoadLevel ("AR firstSene");
+	}
+
+	public void LoadArabicNum()
+	{
+		Application.LoadLevel ("AR ArabicNum Scene");
+	}
+
+	public void LoadEnglishLetter()
+	{
+		Application.LoadLevel ("AR EngLetter Scene");
+	}
+
+	public void LoadEnglishNum()
+	{
+		Application.LoadLevel ("AR EngNum Scene");
+	}
+
+	public void LoadEnglishAnimal()
+	{
+		Application.LoadLevel ("AR EnglishAnimal Scene");
+	}
+
+	public void LoadArabicAnimal()
+	{
+		Application.LoadLevel ("AR ArabicAnimal Scene");
+	}
+}
